298dae9043926db9941b2248c89c90008374c41d
Bart Brouns <bart@magnetophon.nl>
2015-02-22 23:53:04 +0100

    cleanup bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,14 @@
 #!/run/current-system/sw/bin/zsh
-
 echo "Installing .dot"
-
 if [ -d ~/.dot/ ]; then
   echo "~/.dot/ directory already exists: do nothing"
 else
   cd ~/
   git clone https://github.com/magnetophon/.dot.git
   cd ~/.dot/
-  echo "clone submodules"
+  echo "get submodules"
   git submodule update --init --recursive
-  #cd ~/.dot/spf13-vim-3/.spf13-vim-3
-  #git submodule init
-  #git submodule update
-  #cd ~/.dot/spf13-vim-3/.spf13-vim-3/.vim/bundle/vundle
-  if [ ! -e ~/.dot/spf13-vim-3/.spf13-vim-3/.vim/bundle/vundle  ]; then
-      git clone https://github.com/gmarik/vundle.git ~/.dot/spf13-vim-3/.spf13-vim-3/.vim/bundle/vundle
-  else
-    echo "vundle already exists: skip"
-  fi
+  git clone https://github.com/gmarik/vundle.git ~/.dot/spf13-vim-3/.spf13-vim-3/.vim/bundle/vundle
   echo "install vim plugins"
   vim \
     -u ~/.dot/spf13-vim-3/.spf13-vim-3/.vimrc.bundles.default \
@@ -27,16 +17,7 @@ else
     +BundleClean \
     +qall
   cd ~/.dot/
-
-  echo "stow the dotfiles"
+  echo "stow dotfiles"
   for item in common zprezto spf13-vim-3; do stow $item; done;
-#  for item in common zprezto nixos-zsh-completion spf13-vim-3 do stow $item; done;
-#  stow common
-
-#  cd $dot_dir/spf13-vim-3/.spf13-vim-3/
-#  git remote add upstream git@github.com:spf13/spf13-vim.git
-#  cd $dot_dir/zprezto/.zprezto/
-#  git remote add upstream git@github.com:sorin-ionescu/prezto.git
-
 fi
 echo "done! :)"
