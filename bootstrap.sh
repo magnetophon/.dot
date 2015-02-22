66579f392eeb9776d9c0ea16ec5d76f9daa57833
Bart Brouns <bart@magnetophon.nl>
2015-02-22 09:31:03 +0100

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/run/current-system/sw/bin/zsh
+
+echo "Installing .dot"
+
+dot_dir="~/.dot"
+if [ -d $dot_dir ]; then
+  echo "$dot_dir directory already exists: do nothing"
+else
+  cd ~/
+  git clone https://github.com/magnetophon/.dot.git
+  cd $dot_dir
+
+#  git submodule init
+#  git submodule update
+#  for item in common zprezto nixos-zsh-completion spf13-vim-3 do stow $item; done;
+  stow common
+#  cd ~/.vim/bundle/vundle
+#  git checkout master
+#  vim +BundleInstall! +BundleClean +q
+
+#  cd $dot_dir/spf13-vim-3/.spf13-vim-3/
+#  git remote add upstream git@github.com:spf13/spf13-vim.git
+#  cd $dot_dir/zprezto/.zprezto/
+#  git remote add upstream git@github.com:sorin-ionescu/prezto.git
+
+fi
+echo "done! :)"
---
+++ commmon/.config/i3/scripts/actions.zsh
@@ -0,0 +1,16 @@
+#!/bin/sh zsh
+
+#
+
+cd ~/.config/i3/scripts/actions/
+		actions=(*(x))
+		if [[ "$2" = '~~' ]]; then
+			do=$(dmenu $actions)
+		elif [[ -n "$2" ]]; then
+			do="$2 "
+			shift 2
+			do+=$*
+		else
+			do=$(print -l $actions | dmenu -b -i  -nb \#3F3F3F -nf \#FAFAD2 -sb \#ffff00 -sf \#000000 -p Actions:)
+		fi
+		eval $PWD/$do &!
