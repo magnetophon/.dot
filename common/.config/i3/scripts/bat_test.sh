b69626efb5d5b640df3f795fa352a223bde89962
Bart Brouns <bart@magnetophon.nl>
2015-12-08 13:54:59 +0100

    fixup bat_test.sh, automatic hibernate

--- common/.config/i3/scripts/bat_test.sh
+++ common/.config/i3/scripts/bat_test.sh
@@ -5,29 +5,26 @@ SAFE_PERCENT=95  # Still safe at this level.
 DANGER_PERCENT=90  # Warn when battery at this level.
 CRITICAL_PERCENT=85  # Hibernate when battery at this level.
 
-NAGBAR_PID=0
+NAGBAR_PID=
 export DISPLAY=:0.0
 
 function launchNagBar
 {
-    i3-nagbar -m 'Battery low!' -b 'Hibernate!' 'systemctl hibernate' >/dev/null 2>&1 &
-    NAGBAR_PID=$!
+    if [[ $NAGBAR_PID -eq "" ]]; then
+        i3-nagbar -m 'Battery low!' -b 'Hibernate!' 'sudo physlock -s -u $USER & systemctl hibernate' >/dev/null 2>&1 &
+    fi
+    NAGBAR_PID=$(pgrep i3-nagbar)
 }
-
 function killNagBar
 {
-    if [[ $NAGBAR_PID -ne 0 ]]; then
-      ps -p $NAGBAR_PID | grep "i3-nagbar"
-          if [[ $? -eq 0 ]]; then
-              kill $NAGBAR_PID
-          fi
-          NAGBAR_PID=0
+    if [[ $NAGBAR_PID -ne "" ]]; then
+      kill $NAGBAR_PID
+      NAGBAR_PID=
     fi
 }
 
 # main:
 while [ true ]; do
-    killNagBar
     if [[ -n $(acpi -a | grep -i off-line) ]]; then
         rem_bat=$(acpi -b | grep -Eo "[0-9]+%" | grep -Eo "[0-9]+")
         if [[ $rem_bat -gt $SAFE_PERCENT ]]; then
@@ -40,11 +37,13 @@ while [ true ]; do
             fi
             if [[ $rem_bat -le $CRITICAL_PERCENT ]]; then
                 SLEEP_TIME=1
+                sudo physlock -s -u $USER
                 systemctl hibernate
             fi
         fi
     else
         SLEEP_TIME=10
+        killNagBar
     fi
     sleep ${SLEEP_TIME}
 done
