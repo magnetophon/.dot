c0d9fcc24f3cf8b4f955e31efb63c9d4e1f67fe9
Bart Brouns <bart@magnetophon.nl>
2017-03-10 16:11:24 +0100

    remove spf13, add spacemacs

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,16 +8,18 @@ else
   cd ~/.dot/
   echo "get submodules"
   git submodule update --init --recursive
-  git clone https://github.com/gmarik/vundle.git ~/.dot/spf13-vim-3/.spf13-vim-3/.vim/bundle/vundle
-  echo "install vim plugins"
-  vim \
-    -u ~/.dot/spf13-vim-3/.spf13-vim-3/.vimrc.bundles.default \
-    +set nomore \
-    +BundleInstall! \
-    +BundleClean \
-    +qall
+#  git clone https://github.com/gmarik/vundle.git ~/.dot/spf13-vim-3/.spf13-vim-3/.vim/bundle/vundle
+#  echo "install vim plugins"
+#  vim \
+#    -u ~/.dot/spf13-vim-3/.spf13-vim-3/.vimrc.bundles.default \
+#    +set nomore \
+#    +BundleInstall! \
+#    +BundleClean \
+#    +qall
   cd ~/.dot/
   echo "stow dotfiles"
-  for item in common zprezto spf13-vim-3; do stow $item; done;
+  for item in common zprezto; do stow $item; done;
+  mkdir ~/.vim
+  touch ~/.vimrc
 fi
 echo "done! :)"
