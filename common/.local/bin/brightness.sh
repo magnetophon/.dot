e4a83128fe50435d6f2bd0ce18615ad8ab07954d
Bart Brouns <bart@magnetophon.nl>
2017-11-24 13:23:49 +0100

    add brightness.sh script so we can go lower than the default

---
+++ common/.local/bin/brightness.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+DEV=/sys/class/backlight/intel_backlight/brightness
+MAX=`cat /sys/class/backlight/intel_backlight/max_brightness`
+ACT=`cat /sys/class/backlight/intel_backlight/actual_brightness`
+MIN=1
+STEPS=30
+STEP=$((MAX/STEPS))
+# STEP=20
+
+if [ $1 == "+" ]; then
+    if [ $((ACT*2)) -le $MAX ]; then
+        echo $((ACT*2)) > $DEV
+    else
+        echo $MAX > $DEV
+    fi
+elif [ $1 == "-" ]; then
+    if [ $((ACT/2)) -ge $MIN ]; then
+        echo $((ACT/2)) > $DEV
+    else
+        echo $MIN > $DEV
+    fi
+fi
+
+exit 0
