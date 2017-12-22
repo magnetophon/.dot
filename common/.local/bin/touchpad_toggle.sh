585307202237c57edd2a9f13caf89c0907257ce3
Bart Brouns <bart@magnetophon.nl>
2017-12-22 03:58:36 +0100

    touchpad_toggle.sh: enable notifications

--- common/.local/bin/touchpad_toggle.sh
+++ common/.local/bin/touchpad_toggle.sh
@@ -7,10 +7,10 @@ STATE=`xinput list-props $ID|grep 'Device Enabled'|awk '{print $4}'`
 if [ $STATE -eq 1 ]
 then
     xinput disable $ID
-    # echo "Touchpad disabled."
-    # notify-send 'Touchpad' 'Disabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
+    echo "Touchpad disabled."
+    notify-send 'Touchpad' 'Disabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
 else
     xinput enable $ID
-    # echo "Touchpad enabled."
-    # notify-send 'Touchpad' 'Enabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
+    echo "Touchpad enabled."
+    notify-send 'Touchpad' 'Enabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
 fi
