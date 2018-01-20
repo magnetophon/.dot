a77f102980abb4af8b751117556fab633a305877
Bart Brouns <bart@magnetophon.nl>
2018-01-20 15:27:07 +0100

    ranger: add fzf integration

--- common/.config/ranger/commands.py
+++ common/.config/ranger/commands.py
@@ -139,3 +139,32 @@ class bulkrename(Command):
                 self.fm.tags.dump()
         else:
             fm.notify("files have not been retagged")
+
+
+class fzf_select(Command):
+    """
+    :fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        if self.quantifier:
+            # match only directories
+            command="fd -t d | fzf  --no-multi"
+        else:
+            # match files and directories
+            command="fzf  --no-multi"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
