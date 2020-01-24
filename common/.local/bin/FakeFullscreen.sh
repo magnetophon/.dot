d9d19a7339acb6eb1ba3a1ea01c7214c00e8b153
Bart Brouns <bart@magnetophon.nl>
2020-01-24 20:06:16 +0100

    update

--- common/.local/bin/FakeFullscreen.sh
+++ common/.local/bin/FakeFullscreen.sh
@@ -2,7 +2,8 @@
 
 # i3 bar mode hide
 if [ "$(i3-msg -t get_bar_config bar-0 | grep dock)" ]; then
-    i3 [class="^.*"] border pixel  && i3 bar mode invisible
+    i3 [class="^.*"] border pixel  && i3 bar mode hide
+    # i3 [class="^.*"] border pixel  && i3 bar mode invisible
 else
     i3 [class="^.*"] border normal && i3 bar mode dock
 fi
