c7e9420a08c0b4d1d4ef19ec01cb5468e95f9d49
Bart Brouns <bart@magnetophon.nl>
2020-01-25 07:01:23 +0100

    finetune polybar

--- common/.config/polybar/launch.sh
+++ common/.config/polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-kill $(pidof polybar)
+kill "$(pidof polybar)"
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
@@ -13,7 +13,11 @@ polybar bar1 >>/tmp/polybar1.log 2>&1 &
 echo "Bars launched..."
 
 sleep 5
-polybar-msg cmd hide
+
+MODE=$(i3-msg -t get_tree | jq -r '.. | .nodes? | .[]? | select(.window!=null and .focused==true).border')
+if [ "$MODE" != "normal" ]; then
+    polybar-msg cmd hide
+    echo "Bars hidden..."
+fi
 
 
-echo "Bars hidden..."
