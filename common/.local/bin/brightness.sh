23f5545ad22f13444eb8ce16e5de1dad43e32c18
Bart Brouns <bart@magnetophon.nl>
2020-01-25 23:13:32 +0100

    brightness to 0 and back +  polybar

--- common/.local/bin/brightness.sh
+++ common/.local/bin/brightness.sh
@@ -5,18 +5,35 @@ ACT=$(cat /sys/class/backlight/intel_backlight/actual_brightness)
 MIN=4
 
 if [ "$1" == "+" ]; then
+    if [ $ACT -eq 0 ]; then
+        FADE=$((MAX/13))
+        while [ $FADE -gt $MIN ]
+        do
+            FADE=$((22*FADE/23))
+            light -Sr $FADE # bright flash. without this it takes too long to get out of 0 brightness
+            usleep 10000
+            # ACT=$(cat /sys/class/backlight/intel_backlight/actual_brightness)
+        done
+        # light -Sr $MAX/3
+        # usleep 25000 # bright flash. without this it takes too long to get out of 0 brightness
+        light -Sr $MIN
+        exit 0
+    fi
     NEW=$(((ACT/2)*3))
+    if [ $NEW -ge "$MAX" ]; then
+        NEW=$MAX
+    fi
 elif [ "$1" == "-" ]; then
     NEW=$((ACT-(ACT/3)))
+    if [ $NEW -le $MIN ] && [ $NEW -ne 0 ]; then
+        if [ $NEW -eq $((MIN-(MIN/3))) ]; then
+            NEW=0
+        else
+            NEW=$MIN
+        fi
+    fi
 fi
 
-if [ $NEW -ge "$MAX" ]; then
-    NEW=$MAX
-fi
-
-if [ $NEW -le $MIN ]; then
-    NEW=$MIN
-fi
 
 light -Sr $NEW
 
