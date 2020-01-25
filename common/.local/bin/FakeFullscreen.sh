b476596f05f4d2b377cee5618d200940f998b896
Bart Brouns <bart@magnetophon.nl>
2020-01-25 18:02:29 +0100

    visual tweaks

--- common/.local/bin/FakeFullscreen.sh
+++ common/.local/bin/FakeFullscreen.sh
@@ -6,12 +6,12 @@
 MODE=$(i3-msg -t get_tree | jq -r '.. | .nodes? | .[]? | select(.window!=null and .focused==true).border')
 
 if [ $MODE = "normal" ]; then
-    i3 [class="^.*"] border pixel
+    i3 [class="^.*"] border pixel 2
     # i3 bar mode hide
     # i3 bar mode invisible
     polybar-msg cmd hide
 else
-    i3 [class="^.*"] border normal
+    i3 [class="^.*"] border normal 1
     # i3 bar mode dock
     polybar-msg cmd show
 fi
