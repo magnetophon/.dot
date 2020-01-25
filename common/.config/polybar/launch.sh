3d336daa06c8346371107494ebb7db4edab6326a
Bart Brouns <bart@magnetophon.nl>
2020-01-25 05:10:48 +0100

    fixup polybar

--- common/.config/polybar/launch.sh
+++ common/.config/polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+kill $(pidof polybar)
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
@@ -10,6 +10,10 @@ echo "---" | tee -a /tmp/polybar1.log # /tmp/polybar2.log
 polybar bar1 >>/tmp/polybar1.log 2>&1 &
 # polybar bar2 >>/tmp/polybar2.log 2>&1 &
 
+echo "Bars launched..."
+
+sleep 5
 polybar-msg cmd hide
 
-echo "Bars launched..."
+
+echo "Bars hidden..."
