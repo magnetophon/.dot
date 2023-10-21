17e4544c9dd9f2f1640332ed4b64b0d752340b5d
Bart Brouns <bart@magnetophon.nl>
2023-10-21 03:35:10 +0200

    fish tweaks

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -27,6 +27,26 @@ alias doom '/home/bart/.config/emacs/bin/doom'
 alias fh _fzf_search_history
 alias fj __zoxide_zi
 
+alias frga rga-fzf
+
+function rga-fzf
+    set RG_PREFIX 'rga --files-with-matches'
+    if test (count $argv) -gt 1
+        set RG_PREFIX "$RG_PREFIX $argv[1..-2]"
+    end
+    set -l file $file
+    set file (
+        FZF_DEFAULT_COMMAND="$RG_PREFIX '$argv[-1]'" \
+        fzf --sort \
+            --preview='test ! -z {} && \
+                rga --pretty --context 5 {q} {}' \
+            --phony -q "$argv[-1]" \
+            --bind "change:reload:$RG_PREFIX {q}" \
+            --preview-window='50%:wrap'
+    ) && \
+    echo "opening $file" && \
+    open "$file"
+end
 
 # NixOS: get the link to a binary
 function wh
