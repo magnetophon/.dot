a231c2ec8e9eea9c73313d7002993b3751072335
Bart Brouns <bart@magnetophon.nl>
2023-12-23 20:51:42 +0100

    clipster fzf: exact match

--- common/.local/bin/clipster_fzf.sh
+++ common/.local/bin/clipster_fzf.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-clipster -o -n 50000 -0 | fzf --read0 --no-sort --reverse --preview='echo {}' | sed -ze 's/\n$//' | clipster
+clipster -o -n 50000 -0 | fzf --read0 --no-sort --reverse --exact --preview='echo {}' | sed -ze 's/\n$//' | clipster
