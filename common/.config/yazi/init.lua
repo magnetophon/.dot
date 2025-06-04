5998e09d2b74ad41cb17652e09f678ee23832b0c
Bart Brouns <bart@magnetophon.nl>
2025-06-04 17:48:03 +0200

    update yazi, bring back old style tab

--- common/.config/yazi/init.lua
+++ common/.config/yazi/init.lua
@@ -3,3 +3,18 @@ require("zoxide"):setup {
 }
 require("projects"):setup({
                          })
+
+
+-- Old tab bar style https://github.com/sxyazi/yazi/pull/2782
+function Tabs.height() return 0 end
+
+Header:children_add(function()
+    if #cx.tabs == 1 then return "" end
+    local spans = {}
+    for i = 1, #cx.tabs do
+      spans[#spans + 1] = ui.Span(" " .. i .. " ")
+    end
+    spans[cx.tabs.idx]:reverse()
+    return ui.Line(spans)
+end, 9000, Header.RIGHT)
+-- END: Old tab bar style https://github.com/sxyazi/yazi/pull/2782
