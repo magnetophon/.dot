10f2e88e72ab39e0a8d59bf4d3b4e573a4e13ac9
Bart Brouns <bart@magnetophon.nl>
2025-06-04 21:49:34 +0200

    fix yazi tabs with names

--- common/.config/yazi/init.lua
+++ common/.config/yazi/init.lua
@@ -6,15 +6,26 @@ require("projects"):setup({
 
 
 -- Old tab bar style https://github.com/sxyazi/yazi/pull/2782
-function Tabs.height() return 0 end
+-- This implementation, with names came from:
+-- https://github.com/boydaihungst/.config/blob/c696dddf66720cb6cae41db76fe8d59c9f29ed03/yazi/init.lua#L189-L207
+
+function Tabs.height()
+  return 0
+end
 
 Header:children_add(function()
-    if #cx.tabs == 1 then return "" end
-    local spans = {}
-    for i = 1, #cx.tabs do
-      spans[#spans + 1] = ui.Span(" " .. i .. " ")
-    end
-    spans[cx.tabs.idx]:reverse()
-    return ui.Line(spans)
+	if #cx.tabs <= 1 then
+      return ""
+	end
+	local spans = {}
+	for i = 1, #cx.tabs do
+      local name = ya.truncate(string.format(" %d %s ", i, cx.tabs[i].name), { max = 20 })
+      if i == cx.tabs.idx then
+        spans[#spans + 1] = ui.Span(" " .. name .. " "):style(th.tabs.active)
+      else
+        spans[#spans + 1] = ui.Span(" " .. name .. " "):style(th.tabs.inactive)
+      end
+	end
+	return ui.Line(spans)
 end, 9000, Header.RIGHT)
 -- END: Old tab bar style https://github.com/sxyazi/yazi/pull/2782
