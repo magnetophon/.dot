583555d20b3ef17ae44e26bae98067aaabc290c5
Bart Brouns <bart@magnetophon.nl>
2026-03-15 23:03:43 +0100

    format on save

--- common/.doom.d/init.el
+++ common/.doom.d/init.el
@@ -66,8 +66,8 @@
        file-templates    ; auto-snippets for empty files
        ;; fold              ; (nigh) universal code folding
        (format            ; automated prettiness
-        )
-       ;; +onsave)          ; automated prettiness
+        ;; )
+        +onsave)          ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
