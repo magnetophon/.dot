b4f0c926c9766f33c7fb3497cfc6839d24e530fd
Bart Brouns <bart@magnetophon.nl>
2026-03-21 16:34:20 +0100

    picom: use separate configs for inverted/non-inverted screen state
    invert_all.sh restarts picom with the appropriate config on toggle.
    invert.sh starts picom with the correct config based on the flag.
    Add picom-non-inverted.conf without mpv/pqiv counter-inversion rules.

--- common/.local/bin/invert.sh
+++ common/.local/bin/invert.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 #
 # This script will adjust the class name on the window
-# It will allow you to adjust your compton config to match
+# It will allow you to adjust your picom config to match
 #
-# add to compton's config:
+# add to picom's config:
 # invert-color-include = [
 #     "class_g ~= '-Inverted$'"
 #
 
-
-
+flag=/tmp/yazi-screen-inverted
 re="(.*)-Inverted$"
 ID=$(xdotool getactivewindow)
-CLASS=$(xprop -id "$ID"  | grep "WM_CLASS" | awk -F'"' '{print $4}')
+CLASS=$(xprop -id "$ID" | grep "WM_CLASS" | awk -F'"' '{print $4}')
 
-if  [[ $CLASS =~ $re ]]; then
+if [[ $CLASS =~ $re ]]; then
     xdotool set_window --class "${BASH_REMATCH[1]}" "$ID"
     notify-send --expire-time 500 "window normal"
 else
-  xdotool set_window --class "$CLASS-Inverted" "$ID"
-  notify-send --expire-time 500 "window inverted"
+    xdotool set_window --class "$CLASS-Inverted" "$ID"
+    notify-send --expire-time 500 "window inverted"
 fi
 
-
-if ! pgrep -x "picom" > /dev/null
-then
-	picom -b
-    notify-send --expire-time 500 "picom started"
+if ! pgrep -x picom >/dev/null; then
+    if [ -e "$flag" ]; then
+        picom -b
+    else
+        picom --config ~/.config/picom-non-inverted.conf -b
+    fi
 fi
--- common/.local/bin/invert_all.sh
+++ common/.local/bin/invert_all.sh
@@ -5,14 +5,21 @@ css=~/.config/qutebrowser/rotate.css
 xrandr-invert-colors
 # for some drivers xrandr-invert-colors doesn't work:
 # xcalib -i -a
+pkill picom 2>/dev/null
+sleep 0.05
+while pgrep -x picom >/dev/null; do sleep 0.01; done
 
 if [ -e "$flag" ]; then
     rm "$flag"
+    # stop counter-inverting
+    picom --config ~/.config/picom-non-inverted.conf -b
     qutebrowser ':set content.user_stylesheets ""' 2>/dev/null || true
     # sometimes crashes qutebrowser:
     # qutebrowser ":config-list-remove content.user_stylesheets $css" 2>/dev/null || true
 else
     touch "$flag"
+    # start counter-inverting marked windows
+    picom -b
     qutebrowser ':set content.user_stylesheets ~/.config/qutebrowser/rotate.css' 2>/dev/null || true
     # sometimes crashes qutebrowser:
     # qutebrowser ":config-list-add content.user_stylesheets $css" 2>/dev/null || true
