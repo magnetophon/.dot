83e0004a68e7037f828a71d089e1eed0ebfe16dd
Bart Brouns <bart@magnetophon.nl>
2026-03-21 15:28:06 +0100

    auto invert qutebrowser when we invert the screen

--- common/.local/bin/invert_all.sh
+++ common/.local/bin/invert_all.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 flag=/tmp/yazi-screen-inverted
+css=~/.config/qutebrowser/rotate.css
 
-xcalib -i -a
+xrandr-invert-colors
+# for some drivers xrandr-invert-colors doesn't work:
+# xcalib -i -a
 
 if [ -e "$flag" ]; then
     rm "$flag"
+    qutebrowser ':set content.user_stylesheets ""' 2>/dev/null || true
+    # sometimes crashes qutebrowser:
+    # qutebrowser ":config-list-remove content.user_stylesheets $css" 2>/dev/null || true
 else
     touch "$flag"
+    qutebrowser ':set content.user_stylesheets ~/.config/qutebrowser/rotate.css' 2>/dev/null || true
+    # sometimes crashes qutebrowser:
+    # qutebrowser ":config-list-add content.user_stylesheets $css" 2>/dev/null || true
 fi
