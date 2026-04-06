669da731e04294e50ea5332836b265b8f7e3efcd
Bart Brouns <bart@magnetophon.nl>
2026-04-06 22:32:27 +0200

    check if qutebrowser is running

--- common/.local/bin/invert_all.sh
+++ common/.local/bin/invert_all.sh
@@ -13,14 +13,18 @@ if [ -e "$flag" ]; then
     rm "$flag"
     # stop counter-inverting
     picom --config ~/.config/picom-non-inverted.conf -b
-    qutebrowser ':set content.user_stylesheets ""' 2>/dev/null || true
+    if pgrep -f qutebrowser >/dev/null; then
+        qutebrowser ':set content.user_stylesheets ""' 2>/dev/null || true
+    fi
     # sometimes crashes qutebrowser:
     # qutebrowser ":config-list-remove content.user_stylesheets $css" 2>/dev/null || true
 else
     touch "$flag"
     # start counter-inverting marked windows
     picom -b
-    qutebrowser ':set content.user_stylesheets ~/.config/qutebrowser/rotate.css' 2>/dev/null || true
+    if pgrep -f qutebrowser >/dev/null; then
+        qutebrowser ':set content.user_stylesheets ~/.config/qutebrowser/rotate.css' 2>/dev/null || true
+    fi
     # sometimes crashes qutebrowser:
     # qutebrowser ":config-list-add content.user_stylesheets $css" 2>/dev/null || true
 fi
